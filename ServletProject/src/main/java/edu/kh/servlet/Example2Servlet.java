409ceb5ef091aed10ae54155ef66f3b392f3462b
baek-dh <baekdh@iei.or.kr>
2021-11-26 14:54:40 +0900

    프로젝트 업로드

---
+++ ServletProject/src/main/java/edu/kh/servlet/Example2Servlet.java
@@ -0,0 +1,52 @@
+package edu.kh.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Example2Servlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// 전달 받은 값 (== 파라미터)을 얻어와 변수에 저장
+		// -> 파라미터는 모두 String이다!!!
+		String food = req.getParameter("food");
+		String amount = req.getParameter("amount");
+		String message = req.getParameter("message");
+		
+		System.out.println(food);
+		System.out.println(amount);
+		System.out.println(message);
+		
+		
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		
+		String result = food + " / " + amount + " / " + message;
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html lang='ko'>");
+		out.print("<head>");
+		out.print("<title>결과 페이지</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>" + result + "</h1>");
+		out.print("<h3>결과 출력 완료</h3>");
+		out.print("</body>");
+		out.print("</html>");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ ServletProject/src/main/java/edu/kh/servlet/Example3Servlet.java
@@ -0,0 +1,27 @@
+package edu.kh.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Example3Servlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String size = req.getParameter("size");
+		String amount = req.getParameter("amount");
+		String color = req.getParameter("color");
+		
+		System.out.println(size + "사이즈 / " + color + " / " + amount + "개" );
+		
+		
+		
+	
+	
+	}
+	
+}
